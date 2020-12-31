700f648d7b897318a4e11770bcf9a32afe4667ef
farrukhkhanacce <47423491+farrukhkhanacce@users.noreply.github.com>
2021-01-01 00:46:41 +0500

    register

---
+++ SampleWebApi/SampleWebApi/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.UserModels;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManger;
+        private readonly SignInManager<ApplicationUser> _signInManger;
+
+        public UsersController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signinManager )
+        {
+            this._userManger = userManager;
+            this._signInManger = signinManager;
+        }
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login([FromBody]dynamic obj)
+        {
+            return Ok();
+        }
+
+        [Route("register")]
+        [HttpPost]
+        
+        public IActionResult Register(ApplicationUser user)
+        {
+
+            this._userManger.CreateAsync(user);
+            return Ok(); 
+        }
+
+    }
+}
--- SampleWebApi/UserManagement/DBContext/UsersDBContext.cs
+++ SampleWebApi/UserManagement/DBContext/UsersDBContext.cs
@@ -3,13 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UserManagement.UserModels;
 
 namespace UserManagement.DBContext
 {
 
 
     // all user tables 
-    public class UsersDBContext:IdentityDbContext
+    public class UsersDBContext:IdentityDbContext<ApplicationUser>
     {
         public UsersDBContext(DbContextOptions<UsersDBContext> options):base(options)
         { 
