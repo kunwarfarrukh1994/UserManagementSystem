bfac71704fea2645ecf0791cbdd84473f07303e4
farrukhkhanacce <47423491+farrukhkhanacce@users.noreply.github.com>
2021-01-09 06:09:05 +0500

    forgot password and reset password

--- SampleWebApi/SampleWebApi/Controllers/UsersController.cs
+++ SampleWebApi/SampleWebApi/Controllers/UsersController.cs
@@ -98,14 +98,116 @@ namespace SampleWebApi.Controllers
             }
         }
 
+        [HttpPost("forgotpassword")]
+
+        public async Task<IActionResult> ForgotPassword([FromQuery] string email)
+        {
+            var user =  await this._userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var TempPassword = RandomPasswordGenerator.GenerateRandomPassword();
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.Action("ResetPassword", "Users", new { token = token, email = user.Email }, Request.Scheme);
+
+                var message = confirmationLink+ "Password :" + TempPassword;
+                MailMessage mailMessage = new MailMessage("farrukhraj20@gmail.com", "farrukhraj20@gmail.com");
+                mailMessage.Subject = "ResetPassword Link";
+                mailMessage.Body = message;
+
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+
+                client.Credentials = new System.Net.NetworkCredential()
+                {
+                    UserName = "farrukhraj20@gmail.com",
+                    Password = "Farrukh@200"
+                };
+                client.EnableSsl = true;
+                await client.SendMailAsync(mailMessage);
+
+                return Ok("Check Your Emial For Password and then reset it ..");
+
+            }
+            else
+            {
+                var err = new HttpResponseExceptionModel();
+                err.ErrorMessages.Add("Invalid Email Address");
+                return BadRequest(JsonConvert.SerializeObject(err));
+            }
+
+
+        }
+
+        [HttpPost("resetpassword")]
+        public async  Task<IActionResult> ResetPassword([FromBody] ResetPasswordViewModel model)
+        {
+            if (model.token != null)
+            {
+                var user = await this._userManager.FindByEmailAsync(model.Email);
+               if (user != null)
+                {
+                    var result = await this._userManager.ResetPasswordAsync(user, model.token, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return Ok("Password Reset Successfully");
+                    }
+                    else
+                    {
+                        var err = new HttpResponseExceptionModel();
+                        err.ErrorMessages.Add("Something Went Wrong please try again later ");
+                        return BadRequest(JsonConvert.SerializeObject(err));
+                    }
+                }
+                else {
+                    var err = new HttpResponseExceptionModel();
+                    err.ErrorMessages.Add("invalid link ");
+                    return BadRequest(JsonConvert.SerializeObject(err));
+                }
+
+            }
+            else
+            {
+                var err = new HttpResponseExceptionModel();
+                err.ErrorMessages.Add("provide token ");
+                return BadRequest(JsonConvert.SerializeObject(err));
+            }
+
+        }
+
+
+        [HttpPost("changepassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordViewModel model)
+        {
+
+            var user = await this._userManager.FindByIdAsync(model.UserId);
+
+            if(user!=null)
+            {
+               var result= await this._userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+
+                if (result.Succeeded)
+                {
+                    return Ok("Password Changed Successfully");
+                }
+                else
+                {
+                    var err = new HttpResponseExceptionModel();
+                    err.ErrorMessages.Add(result.Errors.Select(op => op.Description).First<string>());
+                    return BadRequest(JsonConvert.SerializeObject(err));
+                }
+
+            }
+            var errr = new HttpResponseExceptionModel();
+            errr.ErrorMessages.Add("Something Went Wrong Please try again later ...");
+            return BadRequest(JsonConvert.SerializeObject(errr));
+
+        }
+
         [HttpPost("register")]
-
-        // [HttpPost]
-
         public async Task<IActionResult> Register(ApplicationUser user)
         {
-            //var TempPassword = RandomPasswordGenerator.GenerateRandomPassword();
-            var TempPassword = "P@ssword123";
+            var TempPassword = RandomPasswordGenerator.GenerateRandomPassword();
+           // var TempPassword = "P@ssword123";
             var result = await this._userManager.CreateAsync(user, TempPassword); // no password 
                                                                                   // var result = await this._userManger.CreateAsync(user,"Password"); // With password 
 
@@ -116,10 +218,7 @@ namespace SampleWebApi.Controllers
                 var confirmationLink = Url.Action("VerifyEmail", "Users", new { token = token, userid = user.Id }, Request.Scheme);
 
 
-                // var message = new string[] { user.Email }, "Confirmation email link", confirmationLink, null;
                 var message = confirmationLink + "\n\n" + "Password :" + TempPassword;
-                //await this._emailservice.SendAsync(user.Email, "Confirmation-Email", message, true);
-
                 MailMessage mailMessage = new MailMessage("farrukhraj20@gmail.com", "farrukhraj20@gmail.com");
                 mailMessage.Subject = "Registration Link";
                 mailMessage.Body = message;
@@ -134,6 +233,8 @@ namespace SampleWebApi.Controllers
                 };
                 client.EnableSsl = true;
                 await client.SendMailAsync(mailMessage);
+
+
                 await _userManager.AddToRoleAsync(user, UserRoles.User);
                 return Ok("Email Sent Successfully ..Check Your Email to Verify ...");
             }
---
+++ SampleWebApi/UserManagement/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace UserManagement.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+       
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The  Password and the Confirm Password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string token { get; set; }
+
+        public ResetPasswordViewModel()
+        {
+                
+        }
+    }
+}
